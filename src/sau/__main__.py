6bf4f15ec27ba813ec0b0f86e1063fb08170621d
Emeka E Ugwuanyi <emylincon@gmail.com>
2025-02-16 21:26:13 +0000

    upgrade to version 12

--- src/sau/__main__.py
+++ src/sau/__main__.py
@@ -26,7 +26,7 @@ from multiprocessing_logging import install_mp_handler
 from typing import Any, Callable, Dict
 
 VERSION = "0.1.0"
-BUILD_DATE = "2025-02-16 21:16"
+BUILD_DATE = "2025-02-16 21:26"
 AUTHOR = "Emeka Ugwuanyi"
 
 
